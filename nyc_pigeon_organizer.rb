b5ba952e024b7a78da3f4fb963fb3f73e5c6c47a
calm-fork-7535 <komald.4@gmail.com>
2020-01-24 05:59:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,11 +7,11 @@
 #         if !new_hash.has_key?(name)
 #           new_hash[name] = {}
 #         end
-# 
+#
 #         if !new_hash[name].has_key?(property)
 #           new_hash[name][property] = []
 #         end
-# 
+#
 #         if !new_hash[name][property].include?(attribute)
 #           new_hash[name][property] << attribute.to_s
 #         end
@@ -28,7 +28,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
-        
+
         if !new_hash[name]
           new_hash[name] = {}
         end
